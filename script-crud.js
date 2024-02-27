0ae489f85b6991ac4ac2ec95bea75788852b1116
conecttheo <theodamata@gmail.com>
2024-02-27 14:28:01 -0300

    ajustes no botao cancelar

--- script-crud.js
+++ script-crud.js
@@ -1,12 +1,17 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
 const paragrafoDescriçãoTarefa = document.querySelector('.app__section-active-task-description')
 
 
-const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+const btnRemoverConcluidas = document.querySelector('#btn-remover-concluidas')
+const btnRemoverTodas = document.querySelector('#btn-remover-todas')
+
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 let tarefaSelecionada = null
+let liTarefaSelecionada = null
 
 function atualizarTarefas () {
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
@@ -39,7 +44,7 @@ function criarElementoTarefa(tarefa) {
             atualizarTarefas()
         }  
     }
-    
+
 
     const imagemBotao = document.createElement('img')
     imagemBotao.setAttribute('src', '/imagens/edit.png')
@@ -49,53 +54,87 @@ function criarElementoTarefa(tarefa) {
     li.append(paragrafo)
     li.append(botao)
 
-    li.onclick = () => {
-        document.querySelectorAll('.app__section-task-list-item-active')
-            .forEach(elemento => {
-                elemento.classList.remove('app__section-task-list-item-active')
-            })
-        if (tarefaSelecionada == tarefa) {
-            paragrafoDescriçãoTarefa.textContent = ''
-            tarefaSelecionada = null
-            return
+    if (tarefa.completa) {
+        li.classList.add('app__section-task-list-item-complete')
+        botao.setAttribute('disabled', 'disabled')
+    } else {
+        li.onclick = () => {
+            document.querySelectorAll('.app__section-task-list-item-active')
+                .forEach(elemento => {
+                    elemento.classList.remove('app__section-task-list-item-active')
+                })
+            if (tarefaSelecionada == tarefa) {
+                paragrafoDescriçãoTarefa.textContent = ''
+                tarefaSelecionada = null
+                liTarefaSelecionada = null
+                return
+            }
+            tarefaSelecionada = tarefa
+            liTarefaSelecionada = li
+            paragrafoDescriçãoTarefa.textContent = tarefa.descricao
+            
+            li.classList.add('app__section-task-list-item-active')
         }
-        tarefaSelecionada = tarefa
-        paragrafoDescriçãoTarefa.textContent = tarefa.descricao
-        
-        li.classList.add('app__section-task-list-item-active')
     }
 
     return li
 }
 
+const limparFormulario = () => {
+    textarea.value = ''; 
+    formAdicionarTarefa.classList.add('hidden');  
+}
 
-// Agora, adicionamos um ouvinte de eventos ao botão. Quando o botão for clicado, esta função será executada.
+btnCancelar.addEventListener('click', limparFormulario);
+
 btnAdicionarTarefa.addEventListener('click', () => {
-    // Esta linha vai alternar a visibilidade do nosso formulário.
     formAdicionarTarefa.classList.toggle('hidden')
 })
-// estamos ouvindo o evento de 'submit' do nosso formulário. 
-// Esse evento ocorre quando tentamos enviar o formulário (geralmente, apertando o botão 'Enter' ou clicando em um botão de submit).
+
 formAdicionarTarefa.addEventListener('submit', (evento) => {
-    // Esta linha evita que a página recarregue (comportamento padrão de um formulário). Nós não queremos isso!
     evento.preventDefault();
     const descricaoTarefa = textarea.value
-    // criamos um objeto tarefa com a descrição vinda da nossa textarea.
     const tarefa = {
         descricao: textarea.value
     }
-    // adicionamos essa tarefa ao nosso array de tarefas.
     tarefas.push(tarefa)
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
-    // E, finalmente, armazenamos nossa lista de tarefas no localStorage. 
-    // Convertendo o array para uma string em formato JSON para poder armazenar.
     atualizarTarefas()
     textarea.value = ''
     formAdicionarTarefa.classList.add('hidden')
 })
 
+
 tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
 });
+
+document.addEventListener('FocoFinalizado', () => {
+    if (tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active')
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete')
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled')
+        tarefaSelecionada.completa = true
+        atualizarTarefas()
+    }
+})
+
+const removerTarefas = (somenteCompletas) => {
+    const seletor = somenteCompletas ? ".app__section-task-list-item-complete" : ".app__section-task-list-item"
+    document.querySelectorAll(seletor).forEach(elemento => {
+        elemento.remove()
+    })
+    tarefas = somenteCompletas ? tarefas.filter(tarefa => !tarefa.completa) : []
+    atualizarTarefas()
+}
+
+btnRemoverConcluidas.onclick = () => removerTarefas(true)
+btnRemoverTodas.onclick = () => removerTarefas(false)
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -77,6 +77,11 @@ const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0) {
         audioTempoFinalizado.play()
         alert('tempo finalizado')
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco'
+        if (focoAtivo) {
+            const evento = new CustomEvent('FocoFinalizado')
+            document.dispatchEvent(evento)
+        }
         zerar()
         return
     }
